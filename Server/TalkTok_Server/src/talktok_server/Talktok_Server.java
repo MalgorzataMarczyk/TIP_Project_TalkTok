1c5ff58d6a9a9e5b53fa5a1b741d25fc1d26f49a
Ewapotranspiracja <32907073+Ewapotranspiracja@users.noreply.github.com>
2019-08-22 13:31:19 +0200

    baza

--- Server/TalkTok_Server/src/talktok_server/Talktok_Server.java
+++ Server/TalkTok_Server/src/talktok_server/Talktok_Server.java
@@ -1,6 +1,10 @@
 package talktok_server;
 
 import java.net.ServerSocket;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
 import java.util.ArrayList;
 
 /**
@@ -13,6 +17,19 @@ public class Talktok_Server {
     private static ServerGUI gui;
 
     public static void main(String[] args) {
+        
+        try{  
+Class.forName("com.mysql.jdbc.Driver");  
+Connection con=DriverManager.getConnection(  
+"jdbc:mysql://localhost:3306/tip?characterEncoding=latin1&useConfigs=maxPerformance","root","password");  
+//root i password musisz ustawic tak jak masz na wlasnej bazie w mysql 
+Statement stmt=con.createStatement();  
+ResultSet rs=stmt.executeQuery("select * from users;");  
+while(rs.next())  
+System.out.println(rs.getInt(1)+"  "+rs.getString(2)+"  "+rs.getString(3));  
+con.close();  
+}catch(Exception e){ System.out.println(e);}
+        
         int portNumber = 3003;
         boolean listening = true;
         int count = 0;
