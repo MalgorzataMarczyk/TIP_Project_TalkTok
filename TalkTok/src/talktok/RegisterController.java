5875cea60996e8961274b1e366ede134c3ea1e9a
MalgorzataMarczykzyk <malgorzata_marczyk@wp.pl>
2019-08-27 22:04:38 +0200

    RegisterWindow
    Add all components and actions to Register window in Application. Also add connection to database and adding user.

--- TalkTok/src/talktok/RegisterController.java
+++ TalkTok/src/talktok/RegisterController.java
@@ -5,7 +5,12 @@
  */
 package talktok;
 
+import java.io.IOException;
 import java.net.URL;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.time.LocalDate;
+import java.util.Date;
 import java.util.ResourceBundle;
 import javafx.application.Platform;
 import javafx.collections.FXCollections;
@@ -18,10 +23,16 @@ import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.CheckBox;
 import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.DatePicker;
 import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
+import javax.swing.JOptionPane;
+import java.sql.*; 
 
 /**
  *
@@ -37,6 +48,32 @@ public class RegisterController implements Initializable {
     @FXML
     private ChoiceBox<String> genders;
     
+    @FXML
+    private PasswordField passwordTextField;
+    
+     @FXML
+    private PasswordField confirmPasswordTextField;
+
+    @FXML
+    private TextField loginTextField;
+    
+    @FXML
+    private TextField emailTextField;
+    
+    @FXML
+    private CheckBox acceptCheckBox;
+    
+     @FXML
+    private DatePicker dateBirthDatePicker;
+    
+    
+    public String userLogin;
+    public String userPassword;
+    public String userEmail;
+    public LocalDate userBirthDate;
+    public String userGender;
+    public boolean dataState = true;
+    public boolean userAdded = false;
     
     
     private void loadData(){
@@ -51,13 +88,123 @@ public class RegisterController implements Initializable {
         
     }
     
+    public void checkoutLogin()throws Exception {
+        if(loginTextField.getText() == null || loginTextField.getText().trim().isEmpty())
+        {
+            JOptionPane.showMessageDialog(null, "Login nie może być pusty!");
+            throw new Exception("User login is empty!");
+            
+        }
+        else{
+             userLogin = loginTextField.getText();
+        }
+         
+    }
+    
+    public Boolean comparePasswords(){
+        String passwordString = passwordTextField.getText();
+        String confPasswordString = confirmPasswordTextField.getText();
+        if(passwordString.compareTo(confPasswordString) == 0){
+            return true;
+        }
+        else
+            return false;
+        
+    }
+    
+    public void checkoutPasswords() throws Exception{
+       if(passwordTextField.getText() == null || passwordTextField.getText().trim().isEmpty())
+      {
+           JOptionPane.showMessageDialog(null, "Hasło nie może być puste!");
+           throw new Exception("User password is empty!");
+      }
+       else if(confirmPasswordTextField.getText() == null || confirmPasswordTextField.getText().trim().isEmpty())
+      {
+           JOptionPane.showMessageDialog(null, "Powtórz hasło");
+           throw new Exception("Confirm password!");
+      }
+       else if (!comparePasswords()){
+           JOptionPane.showMessageDialog(null, "Hasła muszą być takie same!");
+           throw new Exception("Passwords must be the same!");
+       }
+       else{
+          userPassword = passwordTextField.getText(); 
+       }
+    }
+    
+    public void checkoutEmail() throws Exception{
+        if(emailTextField.getText() == null || emailTextField.getText().trim().isEmpty() ){
+            JOptionPane.showMessageDialog(null, "Prosze podać email!");
+            throw new Exception("User email is empty!");
+        }
+        else{
+            userEmail = emailTextField.getText();
+        }
+    }
+    
+    public void checkoutAcceptCheckBox() throws Exception{
+        if(!acceptCheckBox.isSelected()){
+            JOptionPane.showMessageDialog(null, "Aby się zarejestrować trzeba zaakceptować regulamin!");
+            throw new Exception("You must accept statute!");
+        }
+    }
+    
+    public void checkoutBirthDate(){
+        if(dateBirthDatePicker.getValue() == null){
+            userBirthDate = LocalDate.now();
+        }
+        else{
+            userBirthDate = dateBirthDatePicker.getValue();
+        }
+    }
+    
+    public void sendUserToDatabase(String usLogin, String usPassword, String usEmail, LocalDate usBirthDate, String usGender ) throws ClassNotFoundException, SQLException{
+        Class.forName("com.mysql.jdbc.Driver");  
+        Connection con=DriverManager.getConnection( "jdbc:mysql://localhost:3306/tip?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC","root","password");  
+        //here sonoo is database name, root is username and password  
+        Statement stmt=con.createStatement();  
+        int rs=stmt.executeUpdate("INSERT INTO users (username, email, password, salt, birthdate, join_date, last_online_date)\n" +
+"VALUES"+ "('" + usLogin +"','" + usEmail +"','"+ usPassword +"','1','" + usBirthDate + "',CURDATE(),CURDATE());");  
+        con.close();  
+    }
+        
     @FXML
     private void handleButtonAction(ActionEvent event) throws Exception {
-        
       
-       
-        
-       
+      try{
+            dataState = true;
+            checkoutLogin();
+            checkoutPasswords();
+            checkoutEmail();
+            checkoutBirthDate();
+            userGender = genders.getValue();
+            checkoutAcceptCheckBox();
+        }catch(Exception e){
+            dataState = false;
+        }
+      
+      
+      try{
+          if(dataState){
+              sendUserToDatabase(userLogin, userPassword, userEmail, userBirthDate, userGender); 
+              userAdded = true;
+          }
+          else
+              userAdded = false;    
+        }catch(Exception e){ 
+            System.out.println(e);   
+        }
+      if(userAdded){
+          JOptionPane.showMessageDialog(null, "Zostałaś poprawnie dodana!");
+          Parent MainParent = FXMLLoader.load(getClass().getResource("xml/login.fxml"));
+          Scene sceneMain = new Scene(MainParent);
+          Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
+          window.setScene(sceneMain);
+          window.show();
+      }
+      else
+          JOptionPane.showMessageDialog(null, "Konto nie zostało dodane. Spróbuj ponownie.");
+ 
     }
     
     @FXML
@@ -71,6 +218,7 @@ public class RegisterController implements Initializable {
     public void initialize(URL url, ResourceBundle rb) {
         
         loadData();
+        
         
     }   
 }
